c18b7602cb36848ce4b4b495c5cab8f5c202d200
cosswald <4928509+osswald@users.noreply.github.com>
2021-02-06 17:52:37 +0100

    add models Carriage and Engine
    add referenced lookup-models

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Carriage
+from .models import Engine
+from .models import CarriageStatus
+from .models import CarriageHome
+from .models import CarriageType
+from .models import SteamHeating
+from .models import PowerUnit
 
-# Register your models here.
+class CarriageAdmin(admin.ModelAdmin):
+    list_display = ('label', 'uic', 'status')
+
+
+admin.site.register(Carriage, CarriageAdmin)
+admin.site.register(Engine)
+admin.site.register(CarriageStatus)
+admin.site.register(CarriageHome)
+admin.site.register(CarriageType)
+admin.site.register(SteamHeating)
+admin.site.register(PowerUnit)
--- base/models.py
+++ base/models.py
@@ -2,67 +2,88 @@ from django.db import models
 
 
 class CarriageStatus(models.Model):
-    label = models.CharField()
+    label = models.CharField(max_length=200)
     sorting = models.IntegerField()
+
+    def __str__(self):
+        return self.label
 
 
 class CarriageType(models.Model):
-    label = models.CharField()
+    label = models.CharField(max_length=200)
     sorting = models.IntegerField()
+
+    def __str__(self):
+        return self.label
 
 
 class CarriageHome(models.Model):
-    label = models.CharField()
+    label = models.CharField(max_length=200)
     sorting = models.IntegerField()
+
+    def __str__(self):
+        return self.label
 
 
 class PowerUnit(models.Model):
-    label = models.CharField()
+    label = models.CharField(max_length=200)
     sorting = models.IntegerField()
+
+    def __str__(self):
+        return self.label
 
 
 class SteamHeating(models.Model):
-    label = models.CharField()
+    label = models.CharField(max_length=200)
     sorting = models.IntegerField()
+
+    def __str__(self):
+        return self.label
 
 
 class Carriage(models.Model):
-    label = models.CharField()
-    historic_name = models.CharField()
-    description = models.TextField()
-    uic = models.CharField()
-    image = models.ImageField()
-    gross_weight = models.FloatField()
-    seats = models.IntegerField()
+    label = models.CharField(max_length=200)
+    historic_name = models.CharField(max_length=200, blank=True)
+    description = models.TextField(blank=True)
+    uic = models.CharField(max_length=200, blank=True)
+    image = models.ImageField(blank=True, null=True)
+    gross_weight = models.FloatField(blank=True, null=True)
+    seats = models.IntegerField(blank=True, null=True)
     status = models.ForeignKey(CarriageStatus, on_delete=models.CASCADE)
-    type = models.ForeignKey(CarriageType, on_delete=models.CASCADE)
+    carriage_type = models.ForeignKey(CarriageType, on_delete=models.CASCADE)
     home = models.ForeignKey(CarriageHome, on_delete=models.CASCADE)
-    start_year = models.IntegerField()
-    last_revision = models.DateField()
-    next_revision = models.DateField()
-    axles_distance = models.FloatField()
-    length = models.FloatField()
-    manufacturer = models.CharField()
+    start_year = models.IntegerField(blank=True, null=True)
+    last_revision = models.DateField(blank=True, null=True)
+    next_revision = models.DateField(blank=True, null=True)
+    axles_distance = models.FloatField(blank=True, null=True)
+    length = models.FloatField(blank=True, null=True)
+    manufacturer = models.CharField(max_length=200, blank=True)
+
+    def __str__(self):
+        return self.label
 
 
 class Engine(models.Model):
-    label = models.CharField()
-    historic_name = models.CharField()
-    description = models.TextField()
-    uic = models.CharField()
-    image = models.ImageField()
-    gross_weight = models.FloatField()
-    traction_25 = models.IntegerField()
-    traction_30 = models.IntegerField()
+    label = models.CharField(max_length=200)
+    historic_name = models.CharField(max_length=200, blank=True)
+    description = models.TextField(blank=True)
+    uic = models.CharField(max_length=200, blank=True)
+    image = models.ImageField(null=True)
+    gross_weight = models.FloatField(blank=True)
+    traction_25 = models.IntegerField(blank=True)
+    traction_30 = models.IntegerField(blank=True)
     power_unit = models.ForeignKey(PowerUnit, on_delete=models.CASCADE)
     steam_heating = models.ForeignKey(SteamHeating, on_delete=models.CASCADE)
     status = models.ForeignKey(CarriageStatus, on_delete=models.CASCADE)
-    type = models.ForeignKey(CarriageType, on_delete=models.CASCADE)
+    carriage_type = models.ForeignKey(CarriageType, on_delete=models.CASCADE)
     home = models.ForeignKey(CarriageHome, on_delete=models.CASCADE)
-    start_year = models.IntegerField()
-    last_revision = models.DateField()
-    next_revision = models.DateField()
-    max_speed = models.IntegerField()
-    lup = models.CharField
-    length = models.FloatField()
-    manufacturer = models.CharField()
+    start_year = models.IntegerField(null=True)
+    last_revision = models.DateField(null=True)
+    next_revision = models.DateField(null=True)
+    max_speed = models.IntegerField(blank=True)
+    lup = models.CharField(max_length=200, blank=True)
+    length = models.FloatField(blank=True)
+    manufacturer = models.CharField(max_length=200, blank=True)
+
+    def __str__(self):
+        return self.label

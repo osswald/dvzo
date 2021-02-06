f8552662fa3a52c6be6fcd02e290e9495da1abdd
cosswald <4928509+osswald@users.noreply.github.com>
2021-02-06 14:50:26 +0100

    add setup.py
    also add instructions for installation to readme

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# This is a README file.
-More content to follow.
+# DVZO Betriebsplanung
+## Beschreibung
+TO DO
+
+## Installation
+```
+git clone git@github.com:osswald/dvzo.git
+cd dvzo
+python setup.py install
+```
---
+++ setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+NAME = 'dvzo'
+VERSION = '0.1.0'
+AUTHOR = 'osswald'
+EMAIL = 'christoph.osswald@dvzo.ch'
+DESCRIPTION = 'Betriebsplanung'
+URL = 'https://github.com/osswald/dvzo'
+REQUIRED = [
+    'Django',
+]
+TEST_REQUIRE = [
+    'pytest',
+    'pytest-azurepipelines',
+]
+
+setuptools.setup(
+    name=NAME,
+    version=VERSION,
+    author=AUTHOR,
+    author_email=EMAIL,
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=URL,
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: GNU GPL V3",
+    ],
+    install_requires=REQUIRED,
+    extras_require={
+        'test': TEST_REQUIRE
+    },
+)

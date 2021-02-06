c3beca8431ceb39d8d033f8d515971863ccc528f
Matthias Osswald <m@osswald.li>
2021-02-06 22:45:59 +0100

    Add login form.

--- train_management/views.py
+++ train_management/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def dashboard(request):
     return render(request, "train_management/dashboard.html")
+
+
+def login(request):
+    return render(request, "train_management/login.html")

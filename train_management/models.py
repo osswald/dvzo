3be72be5075e640cb35d801c7c75c4eb4990f407
cosswald <4928509+osswald@users.noreply.github.com>
2021-02-13 18:21:22 +0100

    Cleanup models
    - remove unused field lup
    - rename Type to VehicleType
    - add some missing whitespaces

--- train_management/models.py
+++ train_management/models.py
@@ -16,7 +16,7 @@ class Vehicle(models.Model):
         verbose_name = _("Vehicle")
         verbose_name_plural = _("Vehicles")
 
-    class Type(models.TextChoices):
+    class VehicleType(models.TextChoices):
         ENGINE = "engine", _("Engine")
         CARRIAGE = "carriage", _("Carriage")
 
@@ -48,34 +48,33 @@ class Vehicle(models.Model):
         FRONT_BACK = "both", _("Front and Back")
 
     label = models.CharField(_("label"), max_length=200)
-    historic_name = models.CharField(_("historic_name"),max_length=200, blank=True)
-    description = models.TextField(_("description"),blank=True)
-    uic = models.CharField(_("UIC"),max_length=200, blank=True)
-    image = models.ImageField(_("image"),blank=True, null=True)
-    gross_weight = models.FloatField(_("gross_weight"),blank=True, null=True)
-    seats = models.IntegerField(_("seats"),blank=True, null=True)
-    type = models.CharField(_("vehicle type"),
-        max_length=80, choices=Type.choices)
+    historic_name = models.CharField(_("historic_name"), max_length=200, blank=True)
+    description = models.TextField(_("description"), blank=True)
+    uic = models.CharField(_("UIC"), max_length=200, blank=True)
+    image = models.ImageField(_("image"), blank=True, null=True)
+    gross_weight = models.FloatField(_("gross_weight"), blank=True, null=True)
+    seats = models.IntegerField(_("seats"), blank=True, null=True)
+    vehicle_type = models.CharField(_("vehicle type"),
+                            max_length=80, choices=VehicleType.choices)
     status = models.CharField(_("status"),
-                                  max_length=80, choices=Status.choices)
+                            max_length=80, choices=Status.choices)
     carriage_type = models.CharField(_("carriage_type"),
-                                  max_length=80, choices=CarriageType.choices)
+                            max_length=80, choices=CarriageType.choices)
     home = models.CharField(_("home"),
-                                  max_length=80, choices=Home.choices)
-    start_year = models.IntegerField(_("start year"),blank=True, null=True)
-    last_revision = models.DateField(_("last revision"),blank=True, null=True)
-    next_revision = models.DateField(_("next revision"),blank=True, null=True)
-    axles_distance = models.FloatField(_("axles distance"),blank=True, null=True)
-    length = models.FloatField(_("length"),blank=True, null=True)
-    manufacturer = models.CharField(_("manufacturer"),max_length=200, blank=True)
-    traction_25 = models.IntegerField(_("traction 25 permille"),blank=True)
-    traction_30 = models.IntegerField(_("traction 30 permille"),blank=True)
+                            max_length=80, choices=Home.choices)
+    start_year = models.IntegerField(_("start year"), blank=True, null=True)
+    last_revision = models.DateField(_("last revision"), blank=True, null=True)
+    next_revision = models.DateField(_("next revision"), blank=True, null=True)
+    axles_distance = models.FloatField(_("axles distance"), blank=True, null=True)
+    length = models.FloatField(_("length"), blank=True, null=True)
+    manufacturer = models.CharField(_("manufacturer"), max_length=200, blank=True)
+    traction_25 = models.IntegerField(_("traction 25 permille"), blank=True)
+    traction_30 = models.IntegerField(_("traction 30 permille"), blank=True)
     power_unit = models.CharField(_("power_unit"),
                                   max_length=80, choices=PowerUnit.choices)
     steam_heating = models.CharField(_("steam_heating"),
-                                  max_length=80, choices=SteamHeating.choices)
-    max_speed = models.IntegerField(_("maximum speed"),blank=True)
-    lup = models.CharField(_("length over bumper"),max_length=200, blank=True)
+                                max_length=80, choices=SteamHeating.choices)
+    max_speed = models.IntegerField(_("maximum speed"), blank=True)
 
     def __str__(self):
         return self.label
@@ -97,12 +96,12 @@ class DayPlanning(models.Model):
         EXTRA = "extra", _("Extra")
         OTHER = "other", _("Other")
 
-    label = models.CharField(_("label"),max_length=200)
+    label = models.CharField(_("label"), max_length=200)
     day_planning_type = models.CharField(_("day_planning_type"),
                                   max_length=80, choices=DayPlanningType.choices)
     date = models.DateField(_("date"))
     status = models.CharField(_("status"),
-                                  max_length=80, choices=DayPlanningStatus.choices)
+                                max_length=80, choices=DayPlanningStatus.choices)
     paid = models.BooleanField(_("paid"))
 
     def __str__(self):

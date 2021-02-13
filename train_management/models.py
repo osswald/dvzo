016d4b388bf826af55123c044a32086c120e4680
cosswald <4928509+osswald@users.noreply.github.com>
2021-02-13 16:32:17 +0100

    Adapt models
    * rename TextChoices: use "human readable" attributes for database values
    * rename several classes: some consistency with the naming is helpful

--- train_management/models.py
+++ train_management/models.py
@@ -16,36 +16,36 @@ class Vehicle(models.Model):
         verbose_name = _("Vehicle")
         verbose_name_plural = _("Vehicles")
 
-    class VehicleType(models.TextChoices):
-        ENGINE = "E", _("Engine")
-        CARRIAGE = "C", _("Carriage")
+    class Type(models.TextChoices):
+        ENGINE = "engine", _("Engine")
+        CARRIAGE = "carriage", _("Carriage")
 
-    class CarriageStatus(models.TextChoices):
-        AVAILABLE = "A", _("Available")
-        SERVICE = "S", _("In Servicing")
-        ASK = "B", _("Ask")
+    class Status(models.TextChoices):
+        AVAILABLE = "available", _("Available")
+        SERVICE = "servicing", _("In Servicing")
+        ASK = "ask", _("Ask")
 
     class CarriageType(models.TextChoices):
-        SEATS = "C", _("Seating Car")
-        GASTRO = "W", _("Waggon Restaurant")
-        CARGO = "H", _("Cargo")
+        SEATS = "seating", _("Seating Car")
+        GASTRO = "restaurant", _("Waggon Restaurant")
+        CARGO = "cargo", _("Cargo")
 
-    class CarriageHome(models.TextChoices):
-        BAUMA = "B", _("Bauma")
-        USTER = "U", _("Uster")
-        WALD = "W", _("Wald ZH")
+    class Home(models.TextChoices):
+        BAUMA = "bauma", _("Bauma")
+        USTER = "uster", _("Uster")
+        WALD = "wald", _("Wald ZH")
 
     class PowerUnit(models.TextChoices):
-        STEAM = "S", _("Steam")
-        DIESEL = "D", _("Diesel")
-        ELECTRIC = "E", _("Electric")
-        DIESEL_ELECTRIC = "F", _("Diesel/Electric")
+        STEAM = "steam", _("Steam")
+        DIESEL = "diesel", _("Diesel")
+        ELECTRIC = "electric", _("Electric")
+        DIESEL_ELECTRIC = "dieselelectric", _("Diesel/Electric")
 
     class SteamHeating(models.TextChoices):
-        NO = "A", _("No")
-        FRONT = "B", _("Front")
-        BACK = "C", _("Back")
-        FRONT_BACK = "D", _("Front and Back")
+        NO = "no", _("No")
+        FRONT = "front", _("Front")
+        BACK = "back", _("Back")
+        FRONT_BACK = "both", _("Front and Back")
 
     label = models.CharField(_("label"), max_length=200)
     historic_name = models.CharField(_("historic_name"),max_length=200, blank=True)
@@ -54,14 +54,14 @@ class Vehicle(models.Model):
     image = models.ImageField(_("image"),blank=True, null=True)
     gross_weight = models.FloatField(_("gross_weight"),blank=True, null=True)
     seats = models.IntegerField(_("seats"),blank=True, null=True)
-    vehicle_type = models.CharField(_("vehicle type"),
-        max_length=1, choices=VehicleType.choices)
+    type = models.CharField(_("vehicle type"),
+        max_length=80, choices=Type.choices)
     status = models.CharField(_("status"),
-                                  max_length=1, choices=CarriageStatus.choices)
+                                  max_length=80, choices=Status.choices)
     carriage_type = models.CharField(_("carriage_type"),
-                                  max_length=1, choices=CarriageType.choices)
+                                  max_length=80, choices=CarriageType.choices)
     home = models.CharField(_("home"),
-                                  max_length=1, choices=CarriageHome.choices)
+                                  max_length=80, choices=Home.choices)
     start_year = models.IntegerField(_("start year"),blank=True, null=True)
     last_revision = models.DateField(_("last revision"),blank=True, null=True)
     next_revision = models.DateField(_("next revision"),blank=True, null=True)
@@ -71,9 +71,9 @@ class Vehicle(models.Model):
     traction_25 = models.IntegerField(_("traction 25 permille"),blank=True)
     traction_30 = models.IntegerField(_("traction 30 permille"),blank=True)
     power_unit = models.CharField(_("power_unit"),
-                                  max_length=1, choices=PowerUnit.choices)
+                                  max_length=80, choices=PowerUnit.choices)
     steam_heating = models.CharField(_("steam_heating"),
-                                  max_length=1, choices=SteamHeating.choices)
+                                  max_length=80, choices=SteamHeating.choices)
     max_speed = models.IntegerField(_("maximum speed"),blank=True)
     lup = models.CharField(_("length over bumper"),max_length=200, blank=True)
 
@@ -83,22 +83,26 @@ class Vehicle(models.Model):
 
 class DayPlanning(models.Model):
 
+    class Meta:
+        verbose_name = _("Day planning")
+        verbose_name_plural = _("Day plannings")
+
     class DayPlanningStatus(models.TextChoices):
-        DRAFT = "D", _("Draft")
-        CONFIRMED = "C", _("Confirmed")
-        EXECUTED = "E", _("Executed")
+        DRAFT = "draft", _("Draft")
+        CONFIRMED = "confirmed", _("Confirmed")
+        EXECUTED = "executed", _("Executed")
 
     class DayPlanningType(models.TextChoices):
-        SUNDAY = "S", _("Sunday")
-        EXTRA = "E", _("Extra")
-        OTHER = "O", _("Other")
+        SUNDAY = "sunday", _("Sunday")
+        EXTRA = "extra", _("Extra")
+        OTHER = "other", _("Other")
 
     label = models.CharField(_("label"),max_length=200)
     day_planning_type = models.CharField(_("day_planning_type"),
-                                  max_length=1, choices=DayPlanningType.choices)
+                                  max_length=80, choices=DayPlanningType.choices)
     date = models.DateField(_("date"))
     status = models.CharField(_("status"),
-                                  max_length=1, choices=DayPlanningStatus.choices)
+                                  max_length=80, choices=DayPlanningStatus.choices)
     paid = models.BooleanField(_("paid"))
 
     def __str__(self):
@@ -106,6 +110,11 @@ class DayPlanning(models.Model):
 
 
 class Train(models.Model):
+
+    class Meta:
+        verbose_name = _("Train")
+        verbose_name_plural = _("Trains")
+
     label = models.CharField(_("label"), max_length=200)
     km = models.IntegerField(_("km"), blank=True)
     day_planning = models.ForeignKey(DayPlanning, on_delete=models.CASCADE)
@@ -115,6 +124,11 @@ class Train(models.Model):
 
 
 class TextBlock(models.Model):
+
+    class Meta:
+        verbose_name = _("Text block")
+        verbose_name_plural = _("Text blocks")
+
     title = models.CharField(_("title"), max_length=200)
     content = models.IntegerField(_("content"), blank=True)
     day_planning = models.ForeignKey(DayPlanning, on_delete=models.CASCADE)
@@ -125,6 +139,11 @@ class TextBlock(models.Model):
 
 
 class TrainConfiguration(models.Model):
+
+    class Meta:
+        verbose_name = _("Train configuration")
+        verbose_name_plural = _("Train configurations")
+
     vehicle = models.ForeignKey(Vehicle, null=True, on_delete=models.CASCADE)
     train = models.ForeignKey(Train, on_delete=models.CASCADE)
     sorting = models.IntegerField(_("sorting"), blank=True, null=True)
@@ -132,16 +151,20 @@ class TrainConfiguration(models.Model):
 
 class Personnel(models.Model):
 
+    class Meta:
+        verbose_name = _("Personnel")
+        verbose_name_plural = _("Personnel")
+
     class PersonnelStatus(models.TextChoices):
-        ACTIVE = "A", _("Active")
-        INACTIVE = "I", _("Inactive")
+        ACTIVE = "active", _("Active")
+        INACTIVE = "inactive", _("Inactive")
 
     firstname = models.CharField(_("firstname"), max_length=200)
     lastname = models.CharField(_("lastname"), max_length=200)
     email = models.CharField(_("email"), max_length=200)
     mobile_phone = models.CharField(_("mobile phone"), max_length=200)
     status = models.CharField(_("status"),
-                                  max_length=1, choices=PersonnelStatus.choices)
+                                  max_length=80, choices=PersonnelStatus.choices)
     mobile_phone_public = models.BooleanField(_("mobile phone publicly available"))
     date_of_birth = models.DateField(_("date of birth"))
 
@@ -151,14 +174,18 @@ class Personnel(models.Model):
 
 class Function(models.Model):
 
+    class Meta:
+        verbose_name = _("Function")
+        verbose_name_plural = _("Functions")
+
     class FunctionType(models.TextChoices):
-        TRAIN = "T", _("Train")
-        BAUMA = "B", _("Bauma")
-        NEUTHAL = "N", _("Neuthal")
-        BAERETSWIL = "X", _("Bäretswil")
-        HINWIL = "H", _("Hinwil")
+        TRAIN = "train", _("Train")
+        BAUMA = "bauma", _("Bauma")
+        NEUTHAL = "neuthal", _("Neuthal")
+        BAERETSWIL = "baeretswil", _("Bäretswil")
+        HINWIL = "hinwil", _("Hinwil")
 
     label = models.CharField(_("label"), max_length=200)
-    label_short = models.CharField(_("label short"), max_length=10)
+    label_short = models.CharField(_("label short"), max_length=800)
     function_type = models.CharField(_("function_type"),
-                                  max_length=1, choices=FunctionType.choices)
+                                  max_length=80, choices=FunctionType.choices)

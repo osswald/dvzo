e68905cfe49279bfcde4840e945f5865e5e1798d
Matthias Osswald <m@osswald.li>
2021-02-20 22:19:08 +0100

    Add form to update a dayplanning.

--- train_management/views.py
+++ train_management/views.py
@@ -3,8 +3,10 @@ import json
 
 from django.shortcuts import HttpResponse
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 
+from train_management.forms import DayPlanningForm
 from train_management.models import DayPlanning
 from train_management.models import Train
 
@@ -23,10 +25,19 @@ def day_planning_list(request):
 
 @login_required
 def day_planning_detail(request, day_planning):
+    if request.method == 'POST':
+        day_planning_obj = DayPlanning.objects.get(id=day_planning)
+        form = DayPlanningForm(request.POST, instance=day_planning_obj)
+        if form.is_valid():
+            form.save()
+            return redirect('day-planning-list')
+
+    day_planning_obj = DayPlanning.objects.get(id=day_planning)
+    form = DayPlanningForm(instance=day_planning_obj)
     day_planning = DayPlanning.objects.get(id=day_planning)
     return render(
         request, "train_management/day_planning_detail.html",
-        {'day_planning': day_planning})
+        {'day_planning': day_planning_obj, 'form': form})
 
 
 def get_day_planning_data():

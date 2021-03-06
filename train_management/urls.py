d13c8a52f32ba5a50d4c053f1c4c7e4160c8701f
cosswald <4928509+osswald@users.noreply.github.com>
2021-03-06 19:52:11 +0100

    Add dayplanning detail page
    For the working process it is easier to have an overview page for
    the day plannings

--- train_management/urls.py
+++ train_management/urls.py
@@ -19,7 +19,8 @@ from . import views
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('dayplanning/', views.DayPlanningListView.as_view(), name='day-planning-list'),
-    path('dayplanning/<int:pk>/', views.DayPlanningUpdateView.as_view(), name='day-planning-detail'),
+    path('dayplanning/detail/<int:pk>/', views.DayPlanningDetailView.as_view(), name='day-planning-detail'),
+    path('dayplanning/update/<int:pk>/', views.DayPlanningUpdateView.as_view(), name='day-planning-update'),
     path('dayplanning/add/', views.DayPlanningCreateView.as_view(), name='day-planning-create'),
     path('dayplanning/<int:pk>/delete/', views.DayPlanningDeleteView.as_view(), name='day-planning-delete'),
 

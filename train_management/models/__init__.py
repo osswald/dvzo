355b544d709dcd8cb8a2595d0d1fdf2f5ba3b5f5
Matthias Osswald <m@osswald.li>
2021-04-05 09:42:35 +0200

    Fix import order issue in models.

--- train_management/models/__init__.py
+++ train_management/models/__init__.py
@@ -1,11 +1,11 @@
-from train_management.models.dayplanning import *  # noqa
-from train_management.models.dvzofunction import *  # noqa
-from train_management.models.functionpersons import *  # noqa
-from train_management.models.mileage import *  # noqa
-from train_management.models.personnel import *  # noqa
-from train_management.models.phonenumber import *  # noqa
-from train_management.models.station import *  # noqa
-from train_management.models.train import *  # noqa
-from train_management.models.traintimetable import *  # noqa
-from train_management.models.traintimetabletemplate import *  # noqa
-from train_management.models.vehicle import *  # noqa
+from train_management.models.personnel import *  # noqa  # isort:skip
+from train_management.models.dvzofunction import *  # noqa  # isort:skip
+from train_management.models.functionpersons import *  # noqa  # isort:skip
+from train_management.models.vehicle import *  # noqa  # isort:skip
+from train_management.models.dayplanning import *  # noqa  # isort:skip
+from train_management.models.train import *  # noqa  # isort:skip
+from train_management.models.station import *  # noqa  # isort:skip
+from train_management.models.traintimetabletemplate import *  # noqa  # isort:skip
+from train_management.models.traintimetable import *  # noqa  # isort:skip
+from train_management.models.mileage import *  # noqa  # isort:skip
+from train_management.models.phonenumber import *  # noqa  # isort:skip

15a2c0fd589c0661bdc07e76a37a53a5627f4b59
cosswald <4928509+osswald@users.noreply.github.com>
2021-04-26 10:29:11 +0200

    add more data with db_helper

--- db_helper.py
+++ db_helper.py
@@ -10,6 +10,9 @@ from train_management.tests.helpers import SetupHelper  # noqa
 
 setup_helper = SetupHelper()
 setup_helper.create_train_components()
+setup_helper.create_station()
+setup_helper.create_function()
+setup_helper.create_train_timetable_template()
 
 day_planning_1 = setup_helper.get_day_planning()
 train_1 = setup_helper.get_train(day_planning_1)
@@ -18,7 +21,6 @@ setup_helper.create_train_config(train_1)
 train_3 = setup_helper.get_train(day_planning_1)
 setup_helper.create_train_config(train_3)
 
-day_planning_2 = setup_helper.get_day_planning(
-    date_str="2021-05-22", label="Whatever Planning")
+day_planning_2 = setup_helper.get_day_planning(date_str="2021-05-22", label="Whatever Planning")
 train_2 = setup_helper.get_train(day_planning_2)
 setup_helper.create_train_config(train_2)

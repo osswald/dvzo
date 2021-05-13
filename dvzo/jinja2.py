c114b20fd25ef207ba27c770a0f522fbc45d2425
haustre <m@rcelwuers.ch>
2021-05-13 10:10:06 +0200

    Ignore bandit warning for jinja2 autoescape

--- dvzo/jinja2.py
+++ dvzo/jinja2.py
@@ -7,7 +7,9 @@ def environment(**options):
     # i18n extension
     options['extensions'] = ['jinja2.ext.i18n']
 
-    env = Environment(**options)
+    # https://bandit.readthedocs.io/en/latest/plugins/b701_jinja2_autoescape_false.html
+    # https://docs.djangoproject.com/en/3.2/topics/templates/#module-django.template.backends.django
+    env = Environment(**options)  # nosec
 
     # i18n template functions
     env.install_gettext_callables(

dda88633400268d281ba2e5cfd81a058daa4ccb6
cosswald <4928509+osswald@users.noreply.github.com>
2021-06-01 09:41:50 +0200

    add tinymce with npm
    for the skins we should look for a way to copy them from the node_modules directory to the static directory
    
    the templates which can be used in the editor are saved in the static folder so they can be accessed by the editor

---
+++ assets/javascript/tinymce.js
@@ -0,0 +1,14 @@
+import 'tinymce'
+
+//Themes
+import 'tinymce/themes/silver'
+
+//Icons
+import 'tinymce/icons/default'
+
+//Plugins
+import 'tinymce/plugins/advlist'
+import 'tinymce/plugins/charmap'
+import 'tinymce/plugins/lists'
+import 'tinymce/plugins/table'
+import 'tinymce/plugins/template'
